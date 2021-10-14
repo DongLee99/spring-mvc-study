1bbf4339c630b5769434a12c9a405060ae9f5abc
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-14 13:27:44 +0900

    view분리

--- servlet/src/main/java/hello/servlet/web/WebConfig.java
+++ servlet/src/main/java/hello/servlet/web/WebConfig.java
@@ -1,2 +1,19 @@
-package hello.servlet.web;public class WebConfig {
-}
+//package hello.servlet.web;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterRegistration;
+//
+//@Configuration
+//public class WebConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean logFilter() {
+//        FilterRegistration filterFilterRegistration = (FilterRegistration) new FilterRegistrationBean<Filter>();
+//        filterFilterRegistration.set
+//
+//    }
+//}
