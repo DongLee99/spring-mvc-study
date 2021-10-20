6f66b40aa1c5a189f0dff53197eb0ceea1a19285
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-10-20 10:49:20 +0900

    modelAttribute 사용

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -45,5 +48,35 @@ public class RequestParamController {
                                  @RequestParam(value = "age", required = true) int memberAge) {
         log.info("username = {}, age = {}",memberName, memberAge);
         return "ok"; //responsebody로 인해 뷰가 실행 되지 않고 바디에 담겨서 json
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(value = "username", required = true, defaultValue = "guest") String memberName,
+                                       @RequestParam(value = "age", required = true, defaultValue = "-1") int memberAge) {
+        log.info("username = {}, age = {}",memberName, memberAge);
+        return "ok"; //responsebody로 인해 뷰가 실행 되지 않고 바디에 담겨서 json
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username = {}, age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+//    @ResponseBody
+//    @RequestMapping("/model-attribute-v1")
+//    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+//        return "ok";
+//    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) { // 요청 파라미터 값이 모두 들어있다. 바인딩을 못하면 익셉션이 터진다.
+        return "ok";
     }
 }
