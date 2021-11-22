b4d577311a074eafa5f669817b1fbbe5521ee44e
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-11-22 19:15:06 +0900

    login practice : login 도메인, 서비스, 컨트롤러 구현

---
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,27 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import hello.login.web.login.LoginForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+}
