36ca99cea168eedb4b769ca40920bbdf16652356
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-11-23 11:05:34 +0900

    login practice : Session 적용하기

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,30 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        Object session = sessionManager.getSession(request);
+        assertThat(session).isEqualTo(member);
+
+        sessionManager.expire(request);
+        Object expire = sessionManager.getSession(request);
+        assertThat(expire).isNull();
+    }
+}
