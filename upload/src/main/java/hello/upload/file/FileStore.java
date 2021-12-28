fc22a496599d163f79af297c501eae447e65e55b
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2021-12-28 10:58:29 +0900

    FileUpload Practice : fileUpload

---
+++ upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,52 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    /**
+     *
+     * multipartfile -> uploadFile
+     *
+     */
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        // image.png -> 서버에 저장 하는 파일명 UUID
+        String uuid = UUID.randomUUID().toString();
+        //"qwe-qwe-qwe-qwe.png
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        String ext = originalFilename.substring(pos + 1);
+        return ext;
+    }
+
+}
